fba3c17c212aa4bce53cc13fecf2fd4a9cc7318e
ccalderona <ccalderona@isise.edu.pe>
2023-02-13 23:08:24 -0500

    CMCA&MBDH

---
+++ script.js
@@ -0,0 +1,58 @@
+setTimeout(cambiaMensaje,5300)
+function cambiaMensaje(){
+    document.getElementById("loading-message").innerText = "❣Enviado❣"
+}
+setTimeout(function(){
+    alert("Ponte audifonooooooooos!!!!")
+},6000)
+
+setTimeout(function(){
+    alert("Espera que ando cargandooooo")
+},15000)
+
+setTimeout(function(){
+    alert("Espera que ando cargandooooo dije")
+},15100)
+
+setTimeout(function(){
+    alert("Bueno ya empiezo")
+},15200)
+
+setTimeout(function(){
+    alert("Tak no sé, pera")
+},15300)
+
+setTimeout(function(){
+    alert("YAYA AHORA SE")
+},15400)
+
+setTimeout(function(){
+    alert("Naa te engané jejeje")
+},15300)
+
+setTimeout(function(){ 
+    alert("Bueno ya!")
+},15400)
+
+setTimeout(function(){
+    alert("Hoy en este día especial, quiero decirte cuán afortunado me siento de tenerte a mi lado.")
+},15500)
+
+setTimeout(function(){ 
+    alert("Juntos hemos compartido momentos inolvidables que siempre guardaré en mi corazón")
+},15600)
+
+
+setTimeout(function(){ 
+    alert("TE AMO Y GRACIAS POR DARLE SENTIDO A MI VIDA Y POR HACERME SENTIR QUERIDO CUANDO ESTAMOS JUNTOS.")
+},15700)
+
+setTimeout(function(){ 
+    alert("Suerteeee!, que tengas un excelente día♥")
+},15800)
+
+
+ function PlayAudio(){
+ document.getElementById("musica1").play();
+    }
+
